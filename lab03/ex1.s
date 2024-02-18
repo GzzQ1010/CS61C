bdda6b4e9c1f8022fda2a41e5b6b847b5fcfe402
GzzQ1010 <gzzeng1010@gmail.com>
2024-02-18 14:37:47 -0500

    lab 03 completed

--- lab03/ex1.s
+++ lab03/ex1.s
@@ -1,11 +1,18 @@
+#store subsequent items in the static segment at avalible address. We can use memory in simulator to check address
 .data
 .word 2, 4, 6, 8
+#Define a label 'n' that points to a space in memory initiazed with integer 9. We can also check it in memory, Data section.  
 n: .word 9
 
+#Store subsequent instructuons in the text segment at the next avalible address
 .text
 main:   
     add t0, x0, x0
     addi t1, x0, 1
+    #The la pseudo-instruction typically expands to a sequence of instructions to load a full address into a register, because immediate values that 
+    #can be loaded directly into a register in a single instruction are limited in size due to the fixed length of RISC-V instructions. For most purposes,
+    #la is expanded into a pair of instructions: LUI (Load Upper Immediate) and an ADDI (ADD Immediate) or ORI (OR Immediate), depending on the specific 
+    #requirements and context
     la t3, n
     lw t3, 0(t3)
 fib:    
@@ -21,3 +28,4 @@ finish:
     ecall # print integer ecall
     addi a0, x0, 10
     ecall # terminate ecall
+#The output of this file is 34 which is 9th element of fib sequence
--- lab03/factorial.s
+++ lab03/factorial.s
@@ -22,3 +22,14 @@ main:
 
 factorial:
     # YOUR CODE HERE
+    mv t1, a0
+    li t2,1
+loop:
+    addi t1, t1, -1
+    mul a0, a0, t1
+    bne t1, t2, loop    
+finish:
+    mv t1, x0
+    mv t2, x0
+    jr ra
+     
